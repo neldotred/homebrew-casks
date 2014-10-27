006fa800264838bfc5f637a811523e47131623ec
nel.red <neldotred@users.noreply.github.com>
2014-10-27 22:58:10 +0900

    Add Blender 2.72b

---
+++ Casks/blender.rb
@@ -0,0 +1,10 @@
+class Blender < Cask
+  version '2.72b'
+  sha256 'b015f030c416d4aa09617f33e93bba3429624dbbb203fb4b6e728257e61b3d5c'
+
+  url "http://download.blender.org/release/Blender2.72/blender-#{version}-OSX_10.6-x86_64.zip"
+  homepage 'http://www.blender.org/'
+  license :gpl
+
+  app 'Blender/blender.app'
+end
